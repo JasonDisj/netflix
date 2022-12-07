5c170fc19f9f0d7b6a2e91c3d2943ff351624ade
Jason Di <jasondisj@gmail.com>
2022-12-07 15:46:46 -0800

    Setup header by adding Netflix logo & icons

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,6 @@
+import { BellIcon, SearchIcon } from "@heroicons/react/solid";
+import Link from "next/link";
+
 function Header() {
   return (
     <header>
@@ -10,15 +13,26 @@ function Header() {
         />
 
         <ul className="hidden space-x-4 md:flex">
-          <li>Home</li>
-          <li>TV Shows</li>
-          <li>Movies</li>
-          <li>New & Popular</li>
-          <li>My List</li>
+          <li className="headerLink">Home</li>
+          <li className="headerLink">TV Shows</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">New & Popular</li>
+          <li className="headerLink">My List</li>
         </ul>
       </div>
 
-      <div></div>
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <SearchIcon className="hidden w-6 h-6 sm:inline" />
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="w-6 h-6" />
+        <Link href="/account">
+          <img
+            src="http://rb.gy/g1pwyx"
+            alt=""
+            className="cursor-pointer rounded"
+          />
+        </Link>
+      </div>
     </header>
   );
 }
