0cf504dadbf84475fe152d0d86a699650dea19e2
Jason Di <jasondisj@gmail.com>
2022-12-12 11:20:13 -0800

    Add Image component to display movie poster

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,5 +1,22 @@
-function Thumbnail() {
-  return <div>Thumbnail</div>;
+import Image from "next/image";
+import { Movie } from "../typescript_definition";
+
+interface Props {
+  movie: Movie;
+}
+
+function Thumbnail({ movie }: Props) {
+  return (
+    <div className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105">
+      <Image
+        src={`https://image.tmdb.org/t/p/w500${
+          movie.backdrop_path || movie.poster_path
+        }`}
+        layout="fill"
+        className="rounded-sm object-cover md:rounded"
+      />
+    </div>
+  );
 }
 
 export default Thumbnail;
