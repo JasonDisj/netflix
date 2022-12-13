c0c30b046a55a09479dd7cef06ff749e4d2ddf19
Jason Di <jasondisj@gmail.com>
2022-12-13 11:50:31 -0800

    Apply onClick on More Info btn to show movie modal

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 import { Movie } from "../typescript_definition";
 import { FaPlay } from "react-icons/fa";
 import { InformationCircleIcon } from "@heroicons/react/solid";
+import { useRecoilState } from "recoil";
+import { modalState, movieState } from "../atoms/modalAtom";
 
 interface Props {
   netflixOriginals: Movie[];
@@ -10,6 +12,10 @@ interface Props {
 
 export default function Banner({ netflixOriginals }: Props) {
   const [movie, setMovie] = useState<Movie | null>(null);
+
+  const [showModal, setShowModal] = useRecoilState(modalState);
+
+  const [currentMovie, setCurrentMovie] = useRecoilState(movieState);
 
   useEffect(() => {
     setMovie(
@@ -42,7 +48,13 @@ export default function Banner({ netflixOriginals }: Props) {
           <FaPlay className="h-4 w-4 text-black md:h-7 md:w-7" />
           Play
         </button>
-        <button className="bannerBtn bg-[gray]/70">
+        <button
+          className="bannerBtn bg-[gray]/70"
+          onClick={() => {
+            setShowModal(true);
+            setCurrentMovie(movie);
+          }}
+        >
           More Info
           <InformationCircleIcon className="h-5 w-5 md:h-8 md:w-8" />
         </button>
