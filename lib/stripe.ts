04d1787df2b06852761744a55e79197048edbade
Jason Di <jasondisj@gmail.com>
2022-12-20 17:06:13 -0800

    Create billingPortal func to redirect to stripe customer portal to view invoices and manage payment & subscription

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -20,5 +20,20 @@ const loadCheckout = async (priceId: string) => {
     .catch((error) => console.log(error.message));
 };
 
-export { loadCheckout };
+// To switch or cancel plan
+const billingPortal = async () => {
+  const instance = getFunctions(app, "us-central1");
+  const functionRef = httpsCallable(
+    instance,
+    "ext-firestore-stripe-payments-createPortalLink"
+  );
+
+  await functionRef({
+    returnUrl: `${window.location.origin}/account`,
+  })
+    .then(({ data }: any) => window.location.assign(data.url))
+    .catch((error) => console.log(error.message));
+};
+
+export { loadCheckout, billingPortal };
 export default payments;
