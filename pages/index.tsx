3eaffa9bb800ee8a112bb972bdaee9d41cd91a17
Jason Di <jasondisj@gmail.com>
2022-12-21 14:08:25 -0800

    Fetch from custom useList hook to display my list

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,16 @@
+import { getProducts, Product } from "@stripe/firestore-stripe-payments";
 import Head from "next/head";
-import Image from "next/image";
+import { useRecoilValue } from "recoil";
+import { modalState, movieState } from "../atoms/modalAtom";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
+import Modal from "../components/Modal";
+import Plans from "../components/Plans";
+import Row from "../components/Row";
+import useAuth from "../hooks/useAuth";
+import useList from "../hooks/useList";
+import useSubscription from "../hooks/useSubscription";
+import payments from "../lib/stripe";
 import { Movie } from "../typescript_definition";
 import requests from "../utils/requests";
 
@@ -14,7 +23,7 @@ interface Props {
   adventureMovies: Movie[];
   romanceMovies: Movie[];
   animations: Movie[];
-  // products: Product[];
+  products: Product[]; // import from @stripe/firestore-stripe-payments
 }
 
 const Home = ({
@@ -26,9 +35,24 @@ const Home = ({
   adventureMovies,
   romanceMovies,
   animations,
+  products,
 }: Props) => {
+  const { loading, user } = useAuth();
+  const showModal = useRecoilValue(modalState);
+  const subscription = useSubscription(user);
+  const movie = useRecoilValue(movieState);
+  const list = useList(user?.uid);
+
+  if (loading || subscription === null) return null;
+
+  if (!subscription) return <Plans products={products} />;
+
   return (
-    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
+    <div
+      className={`relative h-screen bg-gradient-to-b lg:h-[140vh] ${
+        showModal && "!h-screen overflow-hidden"
+      }`}
+    >
       <Head>
         <title>Netflix</title>
         <link rel="icon" href="/favicon.ico" />
@@ -37,20 +61,26 @@ const Home = ({
       {/* Header */}
       <Header />
 
-      <main>
+      <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
         {/* Banner */}
         <Banner netflixOriginals={netflixOriginals} />
 
-        <section>
+        <section className="md:space-y-24">
           {/* Row */}
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
+          <Row title="Trending Now" movies={trendingNow} />
+          <Row title="Top Rated" movies={topRated} />
+          {/* My List */}
+          {list.length > 0 && <Row title="My list" movies={list} />}
+          <Row title="Action" movies={actionMovies} />
+          <Row title="Comedy" movies={comedyMovies} />
+          <Row title="Adventure" movies={adventureMovies} />
+          <Row title="Romance" movies={romanceMovies} />
+          <Row title="Animation" movies={animations} />
         </section>
       </main>
 
       {/* Modal */}
+      {showModal && <Modal />}
     </div>
   );
 };
@@ -78,6 +108,13 @@ export const getServerSideProps = async () => {
     fetch(requests.fetchAnimations).then((res) => res.json()),
   ]);
 
+  const products = await getProducts(payments, {
+    includePrices: true,
+    activeOnly: true,
+  })
+    .then((res) => res)
+    .catch((error) => console.log(error.message));
+
   return {
     props: {
       netflixOriginals: netflixOriginals.results,
@@ -88,6 +125,7 @@ export const getServerSideProps = async () => {
       adventureMovies: adventureMovies.results,
       romanceMovies: romanceMovies.results,
       animations: animations.results,
+      products,
     },
   };
 };
